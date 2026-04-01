5fec12b2afb5a09cfb2120eb14c95e26e9db076f
Honger469 <jaslan469@mail.ru>
2026-04-01 18:25:20 +0400

    Автотесты Кинескоп 4 модуль

--- constants.py
+++ constants.py
@@ -4,14 +4,6 @@ BASE_URL_MOVIES = "https://api.dev-cinescope.coconutqa.ru"
 BASE_URL_PAYMENT = "https://payment.dev-cinescope.coconutqa.ru"
 
 
-HEADERS = {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-}
-
-LOGIN_ENDPOINT = "/login"
-REGISTER_ENDPOINT = "/register"
-LOGOUT_ENDPOINT = "/logout"
 
 MOVIE_ENDPOINT = "/movies"
 
