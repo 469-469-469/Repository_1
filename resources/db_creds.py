9fb98c175761be870f800c2bf82b2fc74659f54d
469-469-469 <jas.akhmedov@gmail.com>
2026-04-23 10:37:55 +0400

    Module 7+ci

--- resources/db_creds.py
+++ resources/db_creds.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class MoviesDbCreds:
-    HOST = os.getenv('DB_MOVIES_HOST')
-    PORT = os.getenv('DB_MOVIES_PORT')
-    DATABASE_NAME = os.getenv('DB_MOVIES_NAME')
-    USERNAME = os.getenv('DB_MOVIES_USERNAME')
-    PASSWORD = os.getenv('DB_MOVIES_PASSWORD')
+    HOST = os.environ["DB_MOVIES_HOST"]
+    PORT = int(os.environ["DB_MOVIES_PORT"])
+    DATABASE_NAME = os.environ["DB_MOVIES_NAME"]
+    USERNAME = os.environ["DB_MOVIES_USERNAME"]
+    PASSWORD = os.environ["DB_MOVIES_PASSWORD"]
